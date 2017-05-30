76542dc0e5e40a3877c7cbc26f5683d471ef902c
leoSantana <leo_svxy@hotmail.com>
2017-05-30 16:30:02 -0400

    working with jorge

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,8 @@ import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptHttpModule} from "nativescript-angular/http";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { SIDEDRAWER_DIRECTIVES } from "nativescript-telerik-ui/sidedrawer/angular";
+import { LISTVIEW_DIRECTIVES } from 'nativescript-telerik-ui/listview/angular';
 
 import { FloatBtn2Component} from "./pages/solicitudes-lista/float-btn.component";
 import { ListItemComponent} from "./pages/solicitudes-lista/list-item.component";
@@ -19,8 +21,9 @@ import { routes, navigatableComponents } from "./app.routing";
   NativeScriptRouterModule.forRoot(routes)
   ],
   declarations: [AppComponent,
-  ...navigatableComponents,FloatBtnComponent,ListItemComponent,
-  FloatBtn2Component],
+  ...navigatableComponents,FloatBtnComponent,
+  ListItemComponent,FloatBtn2Component,
+  LISTVIEW_DIRECTIVES,SIDEDRAWER_DIRECTIVES],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- app/pages/perfil/perfil.component.ts
+++ app/pages/perfil/perfil.component.ts
@@ -1,18 +1,40 @@
-import { Component } from "@angular/core";
+import { Component,ViewChild,OnInit} from "@angular/core";
 import { Router } from "@angular/router";
 import { Page} from "ui/page"
+import { ListViewEventData,RadListView} from "nativescript-telerik-ui/listview";
+import { RadSideDrawerComponent,SideDrawerType} from "nativescript-telerik-ui/sidedrawer/angular";
+import { View } from 'ui/core/view';
+import * as Utils from "utils/utils";
+import * as FrameModule from "ui/frame";
+import * as Toast from 'nativescript-toast';
+
 @Component({
     selector: "perfil",
     templateUrl: "./pages/perfil/perfil.html",
     styleUrls: ["pages/perfil/perfil-common.css","pages/perfil/perfil.css"]
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+private drawer:SideDrawerType
+@ViewChild(RadSideDrawerComponent)
+public drawerComponent: RadSideDrawerComponent;
 
      constructor(private router : Router,page:Page){
     
   }
+  public ngOnInit(){
+        this.drawer=this.drawerComponent.sideDrawer;
+    }
 
 guardar(){
     this.router.navigate(["/solicitudes-holder"])
 }
+ public show(){
+        this.drawer.showDrawer();
+        Toast.makeText(" menu item tapped").show();
+    }
+
+     public onMenuTapped(value: any) {
+        Toast.makeText(value + " menu item selected").show();
+        this.drawer.closeDrawer();
+    }
 }
