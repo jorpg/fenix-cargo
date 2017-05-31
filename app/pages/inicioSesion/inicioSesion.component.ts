ee0f8efd32a85c5efe31d9c0d005811f3007d099
Jorge Pernia <jorpg23@gmail.com>
2017-05-31 11:47:12 -0400

    ultimos ajustes

--- app/pages/inicioSesion/inicioSesion.component.ts
+++ app/pages/inicioSesion/inicioSesion.component.ts
@@ -7,12 +7,12 @@ import { Page} from "ui/page"
   templateUrl:"pages/inicioSesion/inicioSesion.html",
   styleUrls: ["pages/inicioSesion/inicioSesion-common.css","pages/inicioSesion/inicioSesion.css"]
 })
+
 export class InicioSesionComponent {
   
   isLogginIn= true;
   constructor(private router : Router,page:Page){
-    
-    page.actionBarHidden = true;
+    page.actionBarHidden = false;
   }
 
   submit(){
@@ -33,4 +33,12 @@ signUp(){
   forgot(){
     this.router.navigate(["/recuperar"])
   }
+    
+}
+export function onShare(args: observable.EventData) {
+    console.log("Share action item tapped.");
+}
+
+export function onDelete(args: observable.EventData) {
+    console.log("Delete action item tapped.");
 }
--- app/pages/terminos/float-btn.component.ts
+++ app/pages/terminos/float-btn.component.ts
@@ -5,34 +5,35 @@ import { Page } from "ui/page";
 @Component({
     selector:"float-btn",
     template:` <StackLayout class="float-btn" orientation="horizontal">
-    <Label class="float-btn-text" text="Cancelar" (tap)=cancelar()></Label>
-    <Label class="float-btn-text" text="Aceptar" (tap)=aceptar()></Label>
-    </StackLayout>
-    `,
-    styles: [` 
-    Button {
-    margin-top: 50%;
-    margin-bottom: 10;
-    width: 250;
-    background-color: #1976D2;
-    color: white;
-    border-radius: 50%;
-    }
-    .float-btn {
-       text-align: center;
-       vertical-align: bottom;
-    background-color: transparent;
-}
-.float-btn-text{
-   background-color: #1976D2;
-   width: 120;
-   height: 40;
-   color: #ffffff;
-   vertical-align: middle;
-   border-radius: 50%;
-   font-size: 20;
-   margin-right: 30;
-}`]
+                  <Button class="float-btn-text" text="Cancelar" (tap)=cancelar() ></Button>
+                  <Button class="float-btn-text" text="Aceptar" (tap)=aceptar() ></Button>
+              </StackLayout>`,
+              
+                styles: [` 
+                Button {
+                margin-top: 50%;
+                margin-bottom: 10;
+                width: 250;
+                background-color: #1976D2;
+                color: white;
+                border-radius: 50%;
+                }
+                .float-btn {
+                  text-align: center;
+                  vertical-align: bottom;
+                background-color: transparent;
+                }
+                .float-btn-text{
+                  background-color: #1976D2;
+                  width: 150;
+                  height: 40;
+                  color: #ffffff;
+                  vertical-align: middle;
+                  border-radius: 50%;
+                  font-size: 16;
+                  margin-left: ;
+                  margin-right: 5;
+                }`]
 
 })
 export class FloatBtnComponent {
