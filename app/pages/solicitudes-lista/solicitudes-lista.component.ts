0e08d1d4ac3133db39f9cb84facbb0107b7f2850
emperator <emperator@android>
2017-06-02 08:53:35 -0400

    adding the sideDrawer in solicitudes-listView left to fix the option perfil

--- app/pages/solicitudes-lista/solicitudes-lista.component.ts
+++ app/pages/solicitudes-lista/solicitudes-lista.component.ts
@@ -1,7 +1,13 @@
-import { Component,OnInit } from "@angular/core";
+import { Component,OnInit,ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { ItemService } from "./direccion.service";
 import {Item}from "./item";
+import { ListViewEventData,RadListView} from "nativescript-telerik-ui/listview";
+import { RadSideDrawerComponent,SideDrawerType} from "nativescript-telerik-ui/sidedrawer/angular";
+import { View } from 'ui/core/view';
+import * as Utils from "utils/utils";
+import * as FrameModule from "ui/frame";
+
 @Component({
     selector: "holder",
     templateUrl: "./pages/solicitudes-lista/solicitudes-lista.html",
@@ -9,14 +15,35 @@ import {Item}from "./item";
 })
 export class SolicitudesListaComponent implements OnInit {
      items: Item[];
+     private drawer:SideDrawerType
+@ViewChild(RadSideDrawerComponent)
+public drawerComponent: RadSideDrawerComponent;
      constructor(private router : Router,private itemService: ItemService){
     this.items = this.itemService.getItems();
+    
  }
 
 detallesItem(){
-    this.router.navigate(["/detalles-item"])
+    this.router.navigate(["/detalles-item"]);
+
 }
  ngOnInit(): void {
+        this.drawer=this.drawerComponent.sideDrawer;
         this.items = this.itemService.getItems();
     }
+    public show(){
+        this.drawer.showDrawer();
+    }
+
+    public home(){
+        this.router.navigate(["/solicitudes-lista"]);
+    }
+
+    public direcciones(){
+        this.router.navigate(["/direccion"]);
+    }
+    public perfil (){
+     this.router.navigate(["/perfil"]);
+    }
+    
 }
